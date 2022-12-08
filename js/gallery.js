e46ef9c3fc984f7b5f39f57b77fd7c6ffd3c4d81
Paul Starkebaum <paulstarkebaum@gmail.com>
2022-12-08 14:07:50 -0300

    Preload images

--- js/gallery.js
+++ js/gallery.js
@@ -1,28 +1,30 @@
+// Activates the image gallery
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
 function activateGallery() {
-	alert("Hello from the gallery file!");
 	let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
 	let mainImage  = document.querySelector("#gallery-photo > img");
-	
-	// Image info to be updated
-	// find DOM elements for title image
-	let galleryInfo = document.querySelector("#gallery-info");
-	let title       = galleryInfo.querySelector(".title");
-	let description = galleryInfo.querySelector(".description");
 
 	thumbnails.forEach(function (thumbnail) {
+    // Preload large images.
+    let newImageSrc = thumbnail.dataset.largeVersion;
+    let newImageAlt = thumbnail.alt;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
 		thumbnail.addEventListener("click", function() {
-			// code to set clicked image as main image
-			let newImageSrc = thumbnail.dataset.largeVersion;
-			let newImageAlt = thumbnail.alt;
+			// code to set clicked image as display image
 			mainImage.setAttribute("src", newImageSrc);
 			mainImage.setAttribute("alt", newImageAlt);
 
-			// Change which image is current
+      // Change image which is current
 			let currentClass = "current"
-			document.querySelector(".current").classList.remove(currentClass);
+      document.querySelector(".current").classList.remove(currentClass);
 			thumbnail.parentNode.classList.add(currentClass);
 
 			// Update image info.
+			let galleryInfo = document.querySelector("#gallery-info");
+			let title       = galleryInfo.querySelector(".title");
+			let description = galleryInfo.querySelector(".description");
 			title.innerHTML       = thumbnail.dataset.title;
 			description.innerHTML = thumbnail.dataset.description;
 		});
